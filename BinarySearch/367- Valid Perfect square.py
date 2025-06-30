d382e07d398d760ede6aaa37159993a86ae6cc4c
PraneshUlleri <ipraneshulleri@gmail.com>
2025-06-30 15:29:59 +0530

    bin serch 367

--- BinarySearch/367- Valid Perfect square.py
+++ BinarySearch/367- Valid Perfect square.py
@@ -1,2 +1,19 @@
 
 
+import math
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        l,r=1,num//2
+        if num==1:
+            return True
+        while(l<r):
+            mid= math.ceil((l+r)/2 )
+            if mid*mid > num:
+                r= mid -1
+            elif mid*mid <num:
+                l=mid
+            elif mid*mid == num:
+                return True
+            else:
+                return False
+        return False
