3bff52cf8c57cf7f96b944ddbdb0e3642273bac6
PraneshUlleri <ipraneshulleri@gmail.com>
2025-10-10 13:35:34 +0530

    744 smallest number greater than the target

---
+++ BinarySearch/744 - Smallest Letter Greater than Target.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        alpha={"a":1,"b":2,"c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15,  "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26}
+        #print (alpha[target])
+        smallest=27
+        smallestchar=target
+        if target=="z":
+            return letters[0]
+        for i in letters:
+            dif=alpha[i] - alpha[target]
+        
+            if (dif< smallest and dif>0):
+                smallest =dif
+                smallestchar=i
+               # print (smallestchar, smallest)
+        if smallestchar==target:
+            return letters[0]
+        return smallestchar
