c19eb9e0daba5bb507fea547b116fdc8039158bb
PraneshUlleri <ipraneshulleri@gmail.com>
2025-10-18 11:42:10 +0530

    119 pascals triangle II

---
+++ DynamicProgram/119- Pascal's Triangle II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex==0:
+            return [1]
+        dp=[[]]*(rowIndex+1)
+        dp =[[0]*(i+1) for i in range(rowIndex+1)]
+        for i in range(0,rowIndex+1):
+            dp[i][0],dp[i][-1]=1,1
+        print(dp)
+        for i in range(2,rowIndex+1):
+            for j in range(1,i):
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+        print (dp)
+        return dp[rowIndex] 
