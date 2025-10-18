8373fbe5ddf5bedf5e143c05e01f1ac1c5e55915
PraneshUlleri <ipraneshulleri@gmail.com>
2025-10-18 10:44:50 +0530

    118 pascal I

---
+++ DynamicProgram/118- Pascal's Triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        dp=[[]]*(numRows)
+        dp = [[0] * (i + 1) for i in range(numRows)]
+        for i in range (numRows):
+            dp[i][0]=1
+            dp[i][-1]=1
+        for i in range (2, numRows):
+            for j in range (1, i):
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+        return (dp)
+
